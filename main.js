5a2268a3814891dbfe177b849e2b89c36a6ceeb1
dionykar <122556892+dionykar@users.noreply.github.com>
2024-02-01 18:43:39 +0200

    Telikh Ypobolh
    Telikh Ypobolh Ergasias

---
+++ main.js
@@ -0,0 +1,19 @@
+function hideOtherLinks(clickedLink) {
+    var links = document.querySelectorAll('a');
+    for (var i = 0; i < links.length; i++) {
+        if (links[i] !== clickedLink) {
+            links[i].style.display = 'none';
+        }
+    }
+}
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") { 
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
